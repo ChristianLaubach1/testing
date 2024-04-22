e8e39876c649c4f78a5cfd25e3f12ad2cc0d4dc8
Christian Laubach <christianlaubach1@gmail.com>
2024-04-22 09:33:22 +0200

    .

--- EC2/lib/ec2-stack.ts
+++ EC2/lib/ec2-stack.ts
@@ -3,6 +3,7 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import { Construct } from 'constructs';
+import { InstanceTarget } from 'aws-cdk-lib/aws-elasticloadbalancingv2-targets';
 
 export class MyEc2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -26,20 +27,25 @@ export class MyEc2Stack extends cdk.Stack {
       ]
     });
 
-    // Create EC2 Instances
-    const instance1 = this.createInstance('MyInstance1', vpc, ec2.SubnetType.PUBLIC);
-    const instance2 = this.createInstance('MyInstance2', vpc, ec2.SubnetType.PUBLIC);
-
     // Create Load Balancer
     const loadBalancer = new elbv2.ApplicationLoadBalancer(this, 'MyLoadBalancer', {
       vpc,
       internetFacing: true,
-      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      vpcSubnets: {
+        subnets: vpc.publicSubnets.slice(0, 2) // Use subnets from different AZs
+      },
       securityGroup: new ec2.SecurityGroup(this, 'LoadBalancerSecurityGroup', {
         vpc,
         allowAllOutbound: true,
       })
     });
+
+    // Create EC2 Instances
+    const instance1Subnet = vpc.publicSubnets[0];
+    const instance2Subnet = vpc.publicSubnets[1];
+
+    const instance1 = this.createInstance('MyInstance1', vpc, instance1Subnet);
+    const instance2 = this.createInstance('MyInstance2', vpc, instance2Subnet);
 
     // Create Target Group
     const targetGroup = new elbv2.ApplicationTargetGroup(this, 'MyTargetGroup', {
@@ -50,8 +56,8 @@ export class MyEc2Stack extends cdk.Stack {
     });
 
     // Add EC2 instances as targets to the target group
-    targetGroup.addTarget(new elbv2.InstanceTarget(instance1));
-    targetGroup.addTarget(new elbv2.InstanceTarget(instance2));
+    targetGroup.addTarget(new InstanceTarget(instance1));
+    targetGroup.addTarget(new InstanceTarget(instance2));
 
     // Create Listener
     loadBalancer.addListener('Listener', {
@@ -66,14 +72,14 @@ export class MyEc2Stack extends cdk.Stack {
     });
   }
 
-  private createInstance(id: string, vpc: ec2.IVpc, subnetType: ec2.SubnetType) {
+  private createInstance(id: string, vpc: ec2.IVpc, subnet: ec2.ISubnet) {
     const instance = new ec2.Instance(this, id, {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
       machineImage: ec2.MachineImage.latestAmazonLinux({
         generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
       }),
       vpc,
-      vpcSubnets: { subnetType },
+      vpcSubnets: { subnets: [subnet] },
       keyName: 'Chris2', // Replace 'achim' with your key pair name
       userData: ec2.UserData.forLinux()
     });
