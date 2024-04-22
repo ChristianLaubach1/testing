12b300b8f694816fa72f0932e97103022c2decfb
Christian Laubach <christianlaubach1@gmail.com>
2024-04-22 10:19:16 +0200

    .

--- EC2/lib/ec2-stack.ts
+++ EC2/lib/ec2-stack.ts
@@ -1,115 +1,100 @@
-import * as cdk from 'aws-cdk-lib/core';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as ssm from 'aws-cdk-lib/aws-ssm';
-import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { InstanceTarget } from 'aws-cdk-lib/aws-elasticloadbalancingv2-targets';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class MyEc2Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // Create VPC
-    const vpc = new ec2.Vpc(this, 'MyVpc', {
-      cidr: '10.0.0.0/16',
-      maxAzs: 2, // Ensure we have at least 2 AZs
-      subnetConfiguration: [
-        {
-          cidrMask: 24,
-          name: 'PublicSubnet1',
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-        {
-          cidrMask: 24,
-          name: 'PublicSubnet2',
-          subnetType: ec2.SubnetType.PUBLIC,
-        }
-      ]
-    });
+        // Create a VPC with specific public subnet configurations
+        const vpc = new ec2.Vpc(this, 'MyVpc', {
+            cidr: '10.0.0.0/16',
+            maxAzs: 3,
+            subnetConfiguration: [
+                {
+                    name: 'PublicSubnet',
+                    subnetType: ec2.SubnetType.PUBLIC,
+                    cidrMask: 24
+                }
+            ]
+        });
 
-    // Create Load Balancer
-    const loadBalancer = new elbv2.ApplicationLoadBalancer(this, 'MyLoadBalancer', {
-      vpc,
-      internetFacing: true,
-      vpcSubnets: {
-        subnets: vpc.publicSubnets.slice(0, 2) // Use subnets from different AZs
-      },
-      securityGroup: new ec2.SecurityGroup(this, 'LoadBalancerSecurityGroup', {
-        vpc,
-        allowAllOutbound: true,
-      })
-    });
+        // Security group for allowing SSH and HTTP access
+        const securityGroup = new ec2.SecurityGroup(this, 'InstanceSecurityGroup', {
+            vpc,
+            description: 'Allow SSH and HTTP inbound',
+            allowAllOutbound: true,
+        });
+        securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH access from anywhere');
+        securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP access from anywhere');
 
-    // Create EC2 Instances
-    const instance1Subnet = vpc.publicSubnets[0];
-    const instance2Subnet = vpc.publicSubnets[1];
+        // IAM role for EC2 instances
+        const role = new iam.Role(this, 'InstanceSSMRole', {
+            assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+            description: 'Role that allows EC2 instances to interact with Systems Manager',
+        });
+        role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
 
-    const instance1 = this.createInstance('MyInstance1', vpc, instance1Subnet);
-    const instance2 = this.createInstance('MyInstance2', vpc, instance2Subnet);
+        // Launch template for EC2 instances
+        const launchTemplate = new ec2.LaunchTemplate(this, 'LaunchTemplate', {
+            machineImage: ec2.MachineImage.latestAmazonLinux({
+                generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+            }),
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+            keyName: 'markus',
+            securityGroup: securityGroup,
+            role: role,
+            userData: ec2.UserData.custom(`#!/bin/bash
+                yum update -y
+                yum install -y httpd php
+                INSTANCE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+                echo "<?php echo 'Server IP Address: $INSTANCE_IP'; ?>" > /var/www/html/index.php
+                systemctl enable httpd
+                systemctl start httpd
+            `)
+        });
 
-    // Create Target Group
-    const targetGroup = new elbv2.ApplicationTargetGroup(this, 'MyTargetGroup', {
-      port: 80,
-      protocol: elbv2.ApplicationProtocol.HTTP,
-      targetType: elbv2.TargetType.INSTANCE,
-      vpc,
-    });
+        // Auto Scaling Group configuration
+        const asg = new autoscaling.AutoScalingGroup(this, 'AutoScalingGroup', {
+            vpc,
+            launchTemplate: launchTemplate,
+            minCapacity: 3,
+            maxCapacity: 6,
+            desiredCapacity: 3,
+            vpcSubnets: {
+                subnetType: ec2.SubnetType.PUBLIC,
+            }
+        });
 
-    // Add EC2 instances as targets to the target group
-    targetGroup.addTarget(new InstanceTarget(instance1));
-    targetGroup.addTarget(new InstanceTarget(instance2));
+        // Application Load Balancer
+        const loadBalancer = new elbv2.ApplicationLoadBalancer(this, 'MyLoadBalancer', {
+            vpc,
+            internetFacing: true,
+        });
 
-    // Create Listener
-    loadBalancer.addListener('Listener', {
-      port: 80,
-      defaultTargetGroups: [targetGroup],
-    });
+        const listener = loadBalancer.addListener('Listener', {
+            port: 80
+        });
+        listener.addTargets('Target', {
+            port: 80,
+            targets: [asg]
+        });
 
-    // Export Load Balancer's DNS name to Systems Manager Parameter Store
-    new ssm.StringParameter(this, 'LoadBalancerDnsName', {
-      parameterName: '/MyEc2Stack/LoadBalancerDnsName',
-      stringValue: loadBalancer.loadBalancerDnsName
-    });
-  }
-
-  private createInstance(id: string, vpc: ec2.IVpc, subnet: ec2.ISubnet) {
-    const instance = new ec2.Instance(this, id, {
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
-      machineImage: ec2.MachineImage.latestAmazonLinux({
-        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
-      }),
-      vpc,
-      vpcSubnets: { subnets: [subnet] },
-      keyName: 'Chris2', // Replace 'achim' with your key pair name
-      userData: ec2.UserData.forLinux()
-    });
-
-    instance.connections.allowFromAnyIpv4(ec2.Port.tcp(22), 'Allow SSH access from anywhere');
-    instance.connections.allowFromAnyIpv4(ec2.Port.tcp(80), 'Allow HTTP access from anywhere');
-
-    instance.userData.addCommands(
-      'yum install -y curl',
-      'curl --silent --location https://rpm.nodesource.com/setup_14.x | sudo bash -',
-      'yum install -y nodejs',
-      'echo "const http = require(\'http\');' +
-      'const server = http.createServer((req, res) => {' +
-      'res.writeHead(200, {\'Content-Type\': \'text/plain\'});' +
-      'const ip = req.connection.remoteAddress;' +
-      'res.end(`Hello, your IP address is: ${ip}`);' +
-      '});' +
-      'server.listen(80, \'0.0.0.0\', () => {' +
-      'console.log(\'Server running at http://0.0.0.0:80/\');' +
-      '});" > /home/ec2-user/server.js',
-      'node /home/ec2-user/server.js &'
-    );
-
-    return instance;
-  }
+        // SSM Parameter for Load Balancer DNS Name
+        new ssm.StringParameter(this, 'LoadBalancerDnsName', {
+            parameterName: '/MyEc2Stack/LoadBalancerDnsName',
+            stringValue: loadBalancer.loadBalancerDnsName
+        });
+    }
 }
 
 const app = new cdk.App();
 new MyEc2Stack(app, 'MyEc2Stack', {
-  env: {
-    region: 'us-east-1'
-  }
+    env: {
+        region: 'us-east-1'
+    }
 });
